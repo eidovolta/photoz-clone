e094b4d7aa5ff82e22a14e72c8fc549d5860c36b
eidovolta <19731799+eidovolta@users.noreply.github.com>
2023-07-17 17:51:34 +0200

    Add Eclipse test comment

--- src/main/java/com/example/eidovolta/photozclone/PhotozcloneApplication.java
+++ src/main/java/com/example/eidovolta/photozclone/PhotozcloneApplication.java
@@ -1,5 +1,7 @@
 package com.example.eidovolta.photozclone;
 
+// Test comment Eclipse
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
